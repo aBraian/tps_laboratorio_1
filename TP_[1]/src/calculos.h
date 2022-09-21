0fa4bcc19e63c4675e6b8136a2814ed73309dcee
aBraian <abraian56@gmail.com>
2022-09-21 20:14:00 -0300

    Documentacion

--- TP_[1]/src/calculos.h
+++ TP_[1]/src/calculos.h
@@ -8,8 +8,31 @@
 #ifndef CALCULOS_H_
 #define CALCULOS_H_
 
+/// @brief Recibe la suma de varias cantidades y lo divide por el número de sumandos
+/// 	   y devuelve por referencia el promedio calculado
+///
+/// @param sumaCantidades Suma de varias cantidades
+/// @param divisor Numero de sumandos
+/// @param promedio Puntero al espacio de memoria donde se guardara el resultado
+/// @return Retorna 0 (EXITO) si obtiene un promedio o -1 (ERROR) si no
 int calcularPromedio(int sumaCantidades, int divisor, float * promedio);
+
+/// @brief Recibe dos valores, uno representa el total, el otro representa la proporcionalidad de una parte
+/// 	   respecto del total, y devuelve por referencia el porcentaje calculado
+///
+/// @param cantidadTotal Valor total de una parte
+/// @param cantidadPromedio Valor proporcional de una parte
+/// @param porcentaje Puntero al espacio de memoria donde se guardara el resultado
+/// @return Retorna 0 (EXITO) si obtiene un porcentaje o -1 (ERROR) si no
 int calcularPorcentaje(int cantidadTotal, int cantidadPromedio, float * porcentaje);
+
+/// @brief Recibe un precio y le calcula un aumento
+///
+/// @param precio Valor sobre el que se calcula el aumento
+/// @param porcentajeInteres Valor del aumento que se va a aplicar
+/// @param aumento Puntero al espacio de memoria donde se guardara el valor del aumento
+/// @param precioConInteres Puntero al espacio de memoria donde se guardara el valor del precio con el aumento aplicado
+/// @return Retorna 0 (EXITO) si obtiene el aumento y el precio con interes o -1 (ERROR) si no
 int calcularInteres(float precio, int porcentajeInteres, float * aumento, float * precioConInteres);
 
 #endif /* CALCULOS_H_ */
--- TP_[1]/src/validaciones.h
+++ TP_[1]/src/validaciones.h
@@ -8,7 +8,26 @@
 #ifndef VALIDACIONES_H_
 #define VALIDACIONES_H_
 
+/// @brief Solicita un numero entero al usuario, luego de verificarlo devuelve el resultado
+///
+/// @param pResultado Puntero al espacio de memoria donde se dejara el resultado de la funcion
+/// @param mensaje Es el mensaje a ser mostrado
+/// @param mensajeError Es el mensaje de Error a ser mostrado
+/// @param minimo Es el numero minimo a ser aceptado
+/// @param maximo Es el numero maximo a ser aceptado
+/// @param reintentos Es la cantidad de veces que se puede equivocar el usuario
+/// @return  Retorna 0 si se obtuvo el numero y -1 si no
 int utn_getInt(int * pResultado, char * mensaje, char * mensajeError, int minimo, int maximo, int reintentos);
+
+/// @brief Solicita un numero flotante al usuario, luego de verificarlo devuelve el resultado
+///
+/// @param pResultado Puntero al espacio de memoria donde se dejara el resultado de la funcion
+/// @param mensaje Es el mensaje a ser mostrado
+/// @param mensajeError Es el mensaje de Error a ser mostrado
+/// @param minimo Es el numero minimo a ser aceptado
+/// @param maximo Es el numero maximo a ser aceptado
+/// @param reintentos Es la cantidad de veces que se puede equivocar el usuario
+/// @return  Retorna 0 si se obtuvo el numero y -1 si no
 int utn_getFloat(float * pResultado, char * mensaje, char * mensajeError, int minimo, int maximo, int reintentos);
 
 #endif /* VALIDACIONES_H_ */
